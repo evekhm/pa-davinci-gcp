698fdcebd8b48d279c73d2b317b1aa533f1aa6f6
Eva Khmelinskaya <evekhm@gmail.com>
2022-11-21 15:02:34 -0800

    initial commit

---
+++ {{cookiecutter.project_id}}/terraform/modules/firebase/variables.tf
@@ -0,0 +1,17 @@
+variable "project_id" {
+  type        = string
+  description = "project ID"
+}
+
+variable "firestore_region" {
+  type        = string
+  description = "Firestore Region - must be app_engine region"
+  # options for firestore: https://cloud.google.com/appengine/docs/locations
+  # {{cookiecutter.gcp_region}} and europe-west1 must be us-central and europe-west for legacy reasons
+  default     = "us-central"
+}
+
+variable "storage_multiregion" {
+  type    = string
+  default = "us"
+}
